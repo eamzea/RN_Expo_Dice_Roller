f30cd3e98d557564d353542d7ffd9a1680f8d4f6
Edgar Zea <cesszea@icloud.com>
2020-07-20 17:24:35 -0500

    DiceRoller App

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [numberState, setNumberState] = useState({
+    uri: require("./src/images/dice1.png"),
+  });
+
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 6 + 1);
+  };
+
+  const playButtonPress = () => {
+    let number = randomNumber();
+
+    switch (number) {
+      case 1:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice1.png"),
+          })
+        );
+        break;
+      case 2:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice2.png"),
+          })
+        );
+        break;
+      case 3:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice3.png"),
+          })
+        );
+        break;
+      case 4:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice4.png"),
+          })
+        );
+        break;
+      case 5:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice5.png"),
+          })
+        );
+        break;
+      case 6:
+        return setNumberState(
+          Object.assign({}, numberState, {
+            uri: require("./src/images/dice6.png"),
+          })
+        );
+        break;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={numberState.uri} />
+      <TouchableOpacity onPress={playButtonPress}>
+        <Text style={styles.gameButton}>Play Game</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E74292",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gameButton: {
+    fontSize: 20,
+    marginTop: 35,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    borderWidth: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+    borderColor: "#FFFFFF",
   },
 });
+
+export default App;
